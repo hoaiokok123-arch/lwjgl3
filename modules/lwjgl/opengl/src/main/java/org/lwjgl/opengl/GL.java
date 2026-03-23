d4a96a580aabb397b5442c792bd7c40365ae62ed
hoaiokok123-arch <hoaiokok123-arch@users.noreply.github.com>
2026-03-23 20:48:38 +0700

    fix(opengl): restore 3.4.1 create() flow in GL

--- modules/lwjgl/opengl/src/main/java/org/lwjgl/opengl/GL.java
+++ modules/lwjgl/opengl/src/main/java/org/lwjgl/opengl/GL.java
@@ -89,24 +89,15 @@ public final class GL {
 
     /** Loads the OpenGL native library, using the default library name. */
     public static void create() {
-        SharedLibrary GL;
-        switch (Platform.get()) {
-            case LINUX:
-                GL = Library.loadNative(GL.class, "org.lwjgl.opengl", Configuration.OPENGL_LIBRARY_NAME, "libGLX.so.0", "libGL.so.1", "libGL.so");
-                break;
-            case MACOSX:
-                // Configuration does not get updated if the value changes, so we have to update it here
-                Configuration.OPENGL_LIBRARY_NAME.set(System.getProperty("org.lwjgl.opengl.libname"));
-                String override = Configuration.OPENGL_LIBRARY_NAME.get();
-                GL = override != null
-                    ? Library.loadNative(GL.class, "org.lwjgl.opengl", override)
-                    : MacOSXLibrary.getWithIdentifier("com.apple.opengl");
-                break;
-            case WINDOWS:
-                GL = Library.loadNative(GL.class, "org.lwjgl.opengl", Configuration.OPENGL_LIBRARY_NAME, "opengl32");
-                break;
-            default:
-                throw new IllegalStateException();
+        SharedLibrary GL = null;
+
+        String contextAPI = Configuration.OPENGL_CONTEXT_API.get();
+
+        boolean tryEGL = "EGL".equals(contextAPI) || (contextAPI == null && isWayland());
+        if (tryEGL) {
+            GL = loadEGL();
+        } else if ("OSMesa".equals(contextAPI)) {
+            GL = loadOSMesa();
         }
 
         if (GL == null) {
